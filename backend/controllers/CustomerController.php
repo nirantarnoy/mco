aa5f478c309beb954ab437c6f6437db9ef0731d4
Niran Wangyad <niran.w@cicnetgrp.net>
2025-07-14 09:48:15 +0700

    udpate

--- backend/controllers/CustomerController.php
+++ backend/controllers/CustomerController.php
@@ -76,7 +76,32 @@ class CustomerController extends Controller
         $model = new Customer();
 
         if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
+            if ($model->load($this->request->post())) {
+                $address = \Yii::$app->request->post('cus_address');
+                $street = \Yii::$app->request->post('cus_street');
+                $district_id = \Yii::$app->request->post('district_id');
+                $city_id = \Yii::$app->request->post('city_id');
+                $province_id = \Yii::$app->request->post('province_id');
+                $zipcode = \Yii::$app->request->post('zipcode');
+
+                $party_type_id = 2;
+
+                if($model->save(false)){
+                    if($address != null || $address != '') {
+
+                            $model_address = new \common\models\AddressInfo();
+                            $model_address->party_id = $model->id;
+                            $model_address->party_type_id = $party_type_id;
+                            $model_address->address = $address;
+                            $model_address->street = $street;
+                            $model_address->district_id = $district_id;
+                            $model_address->city_id = $city_id;
+                            $model_address->province_id = $province_id;
+                            $model_address->zip_code = $zipcode;
+                            $model_address->save(false);
+
+                    }
+                }
                 return $this->redirect(['view', 'id' => $model->id]);
             }
         } else {
@@ -99,7 +124,41 @@ class CustomerController extends Controller
     {
         $model = $this->findModel($id);
 
-        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+        if ($this->request->isPost && $model->load($this->request->post())) {
+            $address = \Yii::$app->request->post('cus_address');
+            $street = \Yii::$app->request->post('cus_street');
+            $district_id = \Yii::$app->request->post('district_id');
+            $city_id = \Yii::$app->request->post('city_id');
+            $province_id = \Yii::$app->request->post('province_id');
+            $zipcode = \Yii::$app->request->post('zipcode');
+
+            $party_type_id = 2;
+
+            if($model->save(false)){
+                if($address != null || $address != '') {
+                    $model_address_check = \common\models\AddressInfo::find()->where(['party_id' => $id,'party_type_id' => $party_type_id])->one();
+                    if($model_address_check) {
+                        $model_address_check->address = $address;
+                        $model_address_check->street = $street;
+                        $model_address_check->district_id = $district_id;
+                        $model_address_check->city_id = $city_id;
+                        $model_address_check->province_id = $province_id;
+                        $model_address_check->zip_code = $zipcode;
+                        $model_address_check->save(false);
+                    }else{
+                        $model_address = new \common\models\AddressInfo();
+                        $model_address->party_id = $id;
+                        $model_address->party_type_id = $party_type_id;
+                        $model_address->address = $address;
+                        $model_address->street = $street;
+                        $model_address->district_id = $district_id;
+                        $model_address->city_id = $city_id;
+                        $model_address->province_id = $province_id;
+                        $model_address->zip_code = $zipcode;
+                        $model_address->save(false);
+                    }
+                }
+            }
             return $this->redirect(['view', 'id' => $model->id]);
         }
 
--- backend/views/customer/_form.php
+++ backend/views/customer/_form.php
@@ -6,6 +6,17 @@ use yii\widgets\ActiveForm;
 /** @var yii\web\View $this */
 /** @var backend\models\Customer $model */
 /** @var yii\widgets\ActiveForm $form */
+$district_data = \backend\models\District::find()->all();
+$city_data = \backend\models\Amphur::find()->all();
+$province_data = \backend\models\Province::find()->all();$district_chk = \backend\models\AddressInfo::findDistrictId($model->id,2); // 1 = vendor 2 = customer
+$city_chk = \backend\models\AddressInfo::findAmphurId($model->id,2);
+$province_chk = \backend\models\AddressInfo::findProvinceId($model->id,2);
+
+$address_chk = \backend\models\AddressInfo::find()->where(['party_id' => $model->id,'party_type_id'=>2])->one();
+
+$x_address = $address_chk == null ? '' : $address_chk->address;
+$x_street = $address_chk == null ? '' : $address_chk->street;
+$x_zipcode = $address_chk == null ? '' : $address_chk->zip_code;
 ?>
 
 <div class="customer-form">
@@ -41,6 +52,101 @@ use yii\widgets\ActiveForm;
         <div class="col-lg-1"></div>
     </div>
 
+    <div class="row">
+        <div class="col-lg-6">
+            <br/>
+            <div class="row">
+                <div class="co-lg-6" style="text-align: center;">
+                    <label for="">ที่อยู่ผู้ขาย</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <label for="">ที่อยู่</label>
+                    <input type="text" class="form-control cus-address" id="cus-address"
+                           value="<?= $model->isNewRecord ? '' : $x_address ?>" name="cus_address">
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <label for="">ถนน</label>
+                    <input type="text" class="form-control cus-street" id="cus-street"
+                           value="<?= $model->isNewRecord ? '' : $x_street ?>" name="cus_street">
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <label for="">ตำบล/แขวง</label>
+                    <select name="district_id" class="form-control district-id" id="district"
+                            onchange="">
+                        <option value="0">--ตำบล/แขวง--</option>
+                        <?php foreach ($district_data as $val): ?>
+                            <?php
+                            $selected = '';
+                            if ($val->DISTRICT_ID == $district_chk)
+                                $selected = 'selected';
+//                    ?>
+                            <option value="<?= $val->DISTRICT_ID ?>" <?= $selected ?>><?= $val->DISTRICT_NAME ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <label for="">อำเภอ/เขต</label>
+                    <select name="city_id" class="form-control city-id" id="city"
+                            onchange="getDistrict($(this))">
+                        <option value="0">--อำเภอ/เขต--</option>
+                        <?php foreach ($city_data as $val2): ?>
+                            <?php
+                            $selected = '';
+                            if ($val2->AMPHUR_ID == $city_chk)
+                                $selected = 'selected';
+//                    ?>
+                            <option value="<?= $val2->AMPHUR_ID ?>" <?= $selected ?>><?= $val2->AMPHUR_NAME ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <label for="">จังหวัด</label>
+                    <select name="province_id" class="form-control province-id" id=""
+                            onchange="getCity($(this))">
+                        <option value="0">--จังหวัด--</option>
+                        <?php foreach ($province_data as $val3): ?>
+                            <?php
+                            $selected = '';
+                            if ($val3->PROVINCE_ID == $province_chk)
+                                $selected = 'selected';
+//                    ?>
+                            <option value="<?= $val3->PROVINCE_ID ?>" <?= $selected ?>><?= $val3->PROVINCE_NAME ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    <label for="">รหัสไปรษณีย์</label>
+                    <input type="text" class="form-control zipcode" id="zipcode"
+                           value="<?= $model->isNewRecord ? '' : $x_zipcode ?>" name="zipcode" readonly>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br />
+
    <div class="row">
        <div class="col-lg-1"></div>
        <div class="col-lg-10">
@@ -55,3 +161,43 @@ use yii\widgets\ActiveForm;
     <?php ActiveForm::end(); ?>
 
 </div>
+<?php
+$url_to_getcity = \yii\helpers\Url::to(['vendor/showcity'], true);
+$url_to_getdistrict = \yii\helpers\Url::to(['vendor/showdistrict'], true);
+$url_to_getzipcode = \yii\helpers\Url::to(['vendor/showzipcode'], true);
+$url_to_getAddress = \yii\helpers\Url::to(['vendor/showaddress'], true);
+
+
+$js = <<<JS
+$(function () {
+    
+});
+function getCity(e){
+    $.post("$url_to_getcity"+"&id="+e.val(),function(data){
+        $("select#city").html(data);
+        $("select#city").prop("disabled","");
+    });
+}
+
+function getDistrict(e){
+    $.post("$url_to_getdistrict"+"&id="+e.val(),function(data){
+                                          $("select#district").html(data);
+                                          $("select#district").prop("disabled","");
+
+                                        });
+                                           $.post("$url_to_getzipcode"+"&id="+e.val(),function(data){
+                                                $("#zipcode").val(data);
+                                              });
+}
+
+function getAddres(e){
+    $.post("$url_to_getAddress"+"&id="+e.val(),function(data){
+        $("#city").html(data);
+        $("select#city").prop("disabled","");
+    });
+}
+JS;
+
+$this->registerJs($js,static::POS_END);
+?>
+
