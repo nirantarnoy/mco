39963073c0c18aa5b0e8e0ae76b302b419dc2870
Niran Wangyad <niran.w@cicnetgrp.net>
2025-08-13 15:13:58 +0700

    update

--- backend/views/purch/_form.php
+++ backend/views/purch/_form.php
@@ -682,11 +682,11 @@ $this->registerJs($dynamicFormJs, \yii\web\View::POS_READY);
 
         <div class="form-group mt-3">
             <div class="d-flex justify-content-between">
-                <?php //if ((!$model::APPROVE_STATUS_APPROVED && !$model::STATUS_CANCELLED) || $model->isNewRecord || $model::APPROVE_STATUS_PENDING): ?>
+                <?php if (($model->approve_status !=1 && $model->status != 3) || $model->isNewRecord): ?>
                     <?= Html::submitButton($model->isNewRecord ? 'สร้างใบขอซื้อ' : 'บันทึกการแก้ไข', [
                         'class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary'
                     ]) ?>
-                <?php //endif; ?>
+                <?php endif; ?>
                 <?= Html::a('ยกเลิก', ['index'], ['class' => 'btn btn-secondary']) ?>
             </div>
         </div>
