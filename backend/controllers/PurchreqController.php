105259f7ca5530a8d1e88cda73bdd64372099c38
Niran Wangyad <niran.w@cicnetgrp.net>
2025-08-18 11:36:17 +0700

    update

--- backend/controllers/PurchreqController.php
+++ backend/controllers/PurchreqController.php
@@ -695,7 +695,7 @@ class PurchreqController extends Controller
                     'name' => $product->name,
                     'code' => $product->code ?? '',
                     'price' => $product->sale_price ?? 0,
-                    'display' => $product->code . ($product->name ? ' (' . $product->name . ')' : '')
+                    'display' => $product->name,// $product->code . ($product->name ? ' (' . $product->name . ')' : '')
                 ];
             }
 
