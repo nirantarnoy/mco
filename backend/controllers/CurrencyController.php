d24b1d95bd47f839aad906e6d7818a49aff249f6
Niran Wangyad <niran.w@cicnetgrp.net>
2025-08-23 10:43:32 +0700

    update

--- backend/controllers/CurrencyController.php
+++ backend/controllers/CurrencyController.php
@@ -25,7 +25,7 @@ class CurrencyController extends Controller
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
-                        'delete' => ['POST'],
+                        'delete' => ['POST','GET'],
                     ],
                 ],
             ]
