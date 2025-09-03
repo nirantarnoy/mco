afa4157903f20cca7b2acd8f20e38819306e3bec
Niran Wangyad <niran.w@cicnetgrp.net>
2025-09-03 10:12:08 +0700

    update

--- backend/views/purch/_form.php
+++ backend/views/purch/_form.php
@@ -759,7 +759,7 @@ $this->registerJs($dynamicFormJs, \yii\web\View::POS_READY);
             </div>
         </div>
         <div class="d-flex justify-content-between">
-            <?php if (($model->approve_status != 1 && $model->status != 3) || $model->isNewRecord): ?>
+            <?php if ( $model->status != 3 || $model->isNewRecord): ?>
                 <?= Html::submitButton($model->isNewRecord ? 'สร้างใบขอซื้อ' : 'บันทึกการแก้ไข', [
                     'class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary'
                 ]) ?>
