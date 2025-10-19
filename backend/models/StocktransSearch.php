9ca441c8368b62e868f8b2ff9defab872f985123
Niran Wangyad <niran.w@cicnetgrp.net>
2025-10-19 17:38:35 +0700

    update

--- backend/models/StocktransSearch.php
+++ backend/models/StocktransSearch.php
@@ -4,12 +4,12 @@ namespace backend\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use backend\models\Stocktrans;
+use backend\models\StockTrans;
 
 /**
  * StocktransSearch represents the model behind the search form of `backend\models\Stocktrans`.
  */
-class StocktransSearch extends Stocktrans
+class StocktransSearch extends StockTrans
 {
     /**
      * {@inheritdoc}
