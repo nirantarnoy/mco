f427cf3804d978c3a7e7ff7fd41423dd89547ffc
Niran Wangyad <niran.w@cicnetgrp.net>
2025-11-14 11:02:29 +0700

    update

--- backend/models/JournalTransSearch.php
+++ backend/models/JournalTransSearch.php
@@ -4,12 +4,12 @@ namespace backend\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\models\JournalTransX;
+use backend\models\JournalTrans;
 
 /**
  * JournalTransSearch represents the model behind the search form of `common\models\JournalTransX`.
  */
-class JournalTransSearch extends JournalTransX
+class JournalTransSearch extends JournalTrans
 {
     public $globalSearch;
     /**
@@ -41,7 +41,7 @@ class JournalTransSearch extends JournalTransX
      */
     public function search($params)
     {
-        $query = JournalTransX::find();
+        $query = JournalTrans::find();
 
         // add conditions that should always apply here
 
