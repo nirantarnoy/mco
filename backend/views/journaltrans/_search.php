e70b2dd26815eccaf497f015c4e3bf8e14426a84
Niran Wangyad <niran.w@cicnetgrp.net>
2025-11-14 11:03:57 +0700

    update

--- backend/views/journaltrans/_search.php
+++ backend/views/journaltrans/_search.php
@@ -23,9 +23,9 @@ use yii\widgets\ActiveForm;
             <!--         <span class="input-group-addon" id="basic-addon1"><i class="fa fa-search"></i></span>-->
             <?= $form->field($model, 'globalSearch')->textInput(['placeholder' => 'ค้นหา', 'class' => 'form-control', 'aria-describedby' => 'basic-addon1'])->label(false) ?>
         </div>
-        <div class="col-lg-2">
-            <?= $form->field($model, 'trans_type_id')->widget(\kartik\select2\Select2::className(),['data'=>\yii\helpers\ArrayHelper::map(\backend\helpers\TransType::asArrayObject(),'id','name'),'options'=>['placeholder'=>'-- ประเภทรายการ --','onchange'=>'$(this).submit()',],'pluginOptions'=>['allowClear'=>true,]])->label(false) ?>
-        </div>
+<!--        <div class="col-lg-2">-->
+<!--            --><?php //= $form->field($model, 'trans_type_id')->widget(\kartik\select2\Select2::className(),['data'=>\yii\helpers\ArrayHelper::map(\backend\helpers\TransType::asArrayObject(),'id','name'),'options'=>['placeholder'=>'-- ประเภทรายการ --','onchange'=>'$(this).submit()',],'pluginOptions'=>['allowClear'=>true,]])->label(false) ?>
+<!--        </div>-->
     </div>
     <?php ActiveForm::end(); ?>
 
