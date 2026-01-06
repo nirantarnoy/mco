ef479c496fbb7fd5b4f43afc8d0695a132392c47
Niran Wangyad <niran.w@cicnetgrp.net>
2026-01-06 16:36:54 +0700

    update

--- backend/views/invoice/print-tax-invoice.php
+++ backend/views/invoice/print-tax-invoice.php
@@ -813,7 +813,7 @@ window.addEventListener('afterprint', function() {
                         <tr>
                             <td style=" padding:8px;text-align:center"><?= $index + 1 ?></td>
                     <td class="text-left" style=" padding:8px;"><?= nl2br(Html::encode($item->item_description)) ?></td>
-                    <td style="padding:8px;text-align:center"><?= number_format($item->quantity, 0) ?> <?= Html::encode($item->unit) ?></td>
+                    <td style="padding:8px;text-align:center"><?= number_format($item->quantity, 0) ?> <?= Html::encode(\backend\models\Unit::findName($item->unit_id)) ?></td>
                             <td class=" text-right" style="padding:8px;"><?= number_format($item->unit_price, 3) ?></td>
                     <td class="text-right" style="padding:8px;"><?= number_format($item->amount, 3) ?></td>
                 </tr>
