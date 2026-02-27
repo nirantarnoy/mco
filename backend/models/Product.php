f1c0f621ac6589fcbc3744757b81f79f73d9d0a1
Niran Wangyad <niran.w@cicnetgrp.net>
2026-02-27 18:38:30 +0700

    update

--- backend/models/Product.php
+++ backend/models/Product.php
@@ -44,27 +44,29 @@ class Product extends \common\models\Product
                 ],
                 'value' => Yii::$app->user->id,
             ],
-            'timestampcby' => [
+            'timestampuby' => [
                 'class' => \yii\behaviors\AttributeBehavior::className(),
                 'attributes' => [
                     ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_by',
                 ],
                 'value' => Yii::$app->user->id,
             ],
-//            'timestampcompany' => [
-//                'class' => \yii\behaviors\AttributeBehavior::className(),
-//                'attributes' => [
-//                    ActiveRecord::EVENT_BEFORE_INSERT => 'company_id',
-//                ],
-//                'value' => isset($_SESSION['user_company_id']) ? $_SESSION['user_company_id'] : 1,
-//            ],
-//            'timestampbranch' => [
-//                'class' => \yii\behaviors\AttributeBehavior::className(),
-//                'attributes' => [
-//                    ActiveRecord::EVENT_BEFORE_INSERT => 'branch_id',
-//                ],
-//                'value' => isset($_SESSION['user_branch_id']) ? $_SESSION['user_branch_id'] : 1,
-//            ],
+            'timestampcompany' => [
+                'class' => \yii\behaviors\AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'company_id',
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'company_id',
+                ],
+                'value' => Yii::$app->session->get('company_id'),
+            ],
+            'timestampbranch' => [
+                'class' => \yii\behaviors\AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'branch_id',
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'branch_id',
+                ],
+                'value' => Yii::$app->session->get('branch_id'),
+            ],
             'timestampupdate' => [
                 'class' => \yii\behaviors\AttributeBehavior::className(),
                 'attributes' => [
