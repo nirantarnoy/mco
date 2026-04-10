3cfd7a82d7a47c319d0082cd3d07a16a18bcacdf
Niran Wangyad <niran.w@cicnetgrp.net>
2026-04-10 10:52:10 +0700

    update

--- backend/views/invoice/print-quotation.php
+++ backend/views/invoice/print-quotation.php
@@ -829,7 +829,7 @@ window.addEventListener('afterprint', function() {
             </div>
             <div class="field-group">
                 <span class="field-label" style="min-width: 160px;">เงื่อนไข / วันที่ครบกำหนด / Credit, Due:</span>
-                <span class="field-value" style="font-size: 18px;"><?= $displayDueDate ? Yii::$app->formatter->asDate($displayDueDate, 'php:d/m/Y') : '' ?></span>
+                <span class="field-value" style="font-size: 18px;padding: 2px 0;"><?= $displayDueDate ? Yii::$app->formatter->asDate($displayDueDate, 'php:d/m/Y') : '' ?></span>
             </div>
         </div>
     </div>
