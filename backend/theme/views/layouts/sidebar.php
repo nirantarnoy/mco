a6f2a7c2b892adf1dc2192902853f974fd73a0e8
Niran Wangyad <niran.w@cicnetgrp.net>
2026-06-12 16:10:00 +0700

    update

--- backend/theme/views/layouts/sidebar.php
+++ backend/theme/views/layouts/sidebar.php
@@ -366,6 +366,12 @@
                                     <p>Payment Voucher</p>
                                 </a>
                             </li>
+                            <li class="nav-item">
+                                <a href="index.php?r=wht/index" class="nav-link wht">
+                                    <i class="far fa-circlez nav-icon"></i>
+                                    <p>จัดการหัก ณ ที่จ่าย (WHT)</p>
+                                </a>
+                            </li>
                             <?php if (\Yii::$app->user->can('purch/index')): ?>
                                 <li class="nav-item">
                                     <a href="index.php?r=purchasemaster" class="nav-link purchasemaster">
