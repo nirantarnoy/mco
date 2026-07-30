9e8dbdf88afa6e9e163920c080b31a439be953fe
Niran Wangyad <niran.w@cicnetgrp.net>
2026-07-30 13:08:42 +0700

    Fix icons disappearing due to aggressive global font-family override

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,7 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/tailwind-theme.css?v=3',
+        'css/tailwind-theme.css?v=4',
     ];
     public $js = [
     ];
