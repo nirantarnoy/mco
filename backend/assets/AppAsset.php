1cdc02caa7cc7391e3ef4275e68aa0c95c6d5e15
Niran Wangyad <niran.w@cicnetgrp.net>
2026-07-30 12:49:07 +0700

    Add Tailwind-like theme CSS override for AdminLTE components

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,6 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/tailwind-theme.css',
     ];
     public $js = [
     ];
