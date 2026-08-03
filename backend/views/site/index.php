7b2472b293a0ad926d056e4708f0e8ca4051df3e
Niran Wangyad <niran.w@cicnetgrp.net>
2026-08-03 15:00:54 +0700

    fix: update dashboard view to use Bootstrap 5 classes

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -23,11 +23,11 @@ $this->registerJsFile('https://code.highcharts.com/modules/exporting.js', ['depe
     <!-- Date Range Filter -->
     <div class="row">
         <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h5 class="panel-title">เลือกช่วงเวลา</h5>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title">เลือกช่วงเวลา</h5>
                 </div>
-                <div class="panel-body">
+                <div class="card-body">
                     <?php $form = ActiveForm::begin(['method' => 'get', 'action' => ['index']]); ?>
                     <div class="row">
                         <div class="col-md-3">
@@ -116,11 +116,11 @@ $this->registerJsFile('https://code.highcharts.com/modules/exporting.js', ['depe
     <div class="row">
         <!-- Price Comparison Chart -->
         <div class="col-md-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h5 class="panel-title">เปรียบเทียบราคาขายกับต้นทุน</h5>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title">เปรียบเทียบราคาขายกับต้นทุน</h5>
                 </div>
-                <div class="panel-body">
+                <div class="card-body">
                     <div id="price-comparison-chart" style="height: 400px;"></div>
                 </div>
             </div>
@@ -128,11 +128,11 @@ $this->registerJsFile('https://code.highcharts.com/modules/exporting.js', ['depe
 
         <!-- Top 10 Products Chart -->
         <div class="col-md-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h5 class="panel-title">สินค้าขายดี 10 อันดับ</h5>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title">สินค้าขายดี 10 อันดับ</h5>
                 </div>
-                <div class="panel-body">
+                <div class="card-body">
                     <div id="top-products-chart" style="height: 400px;"></div>
                 </div>
             </div>
@@ -143,23 +143,23 @@ $this->registerJsFile('https://code.highcharts.com/modules/exporting.js', ['depe
     <!-- Sales by Product Table -->
     <div class="row">
         <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h5 class="panel-title">รายงานยอดขายแยกตามสินค้า</h5>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title">รายงานยอดขายแยกตามสินค้า</h5>
                 </div>
-                <div class="panel-body">
+                <div class="card-body">
                     <div class="table-responsive">
                         <table class="table table-bordered table-hover">
                             <thead>
                             <tr>
                                 <th>#</th>
                                 <th>ชื่อสินค้า</th>
-                                <th class="text-right">จำนวนขาย</th>
-                                <th class="text-right">ยอดขาย</th>
-                                <th class="text-right">ราคาขายเฉลี่ย</th>
-                                <th class="text-right">ต้นทุน</th>
-                                <th class="text-right">กำไร</th>
-                                <th class="text-right">%กำไร</th>
+                                <th class="text-end">จำนวนขาย</th>
+                                <th class="text-end">ยอดขาย</th>
+                                <th class="text-end">ราคาขายเฉลี่ย</th>
+                                <th class="text-end">ต้นทุน</th>
+                                <th class="text-end">กำไร</th>
+                                <th class="text-end">%กำไร</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -167,17 +167,17 @@ $this->registerJsFile('https://code.highcharts.com/modules/exporting.js', ['depe
                                 <tr>
                                     <td><?= $index + 1 ?></td>
                                     <td><?= Html::encode($product['name']) ?></td>
-                                    <td class="text-right"><?= number_format($product['total_qty']) ?></td>
-                                    <td class="text-right">฿<?= number_format($product['total_sales'], 2) ?></td>
-                                    <td class="text-right">฿<?= number_format($product['avg_price'], 2) ?></td>
-                                    <td class="text-right">฿<?= number_format($product['cost_price'], 2) ?></td>
-                                    <td class="text-right">฿<?= number_format($product['profit'], 2) ?></td>
-                                    <td class="text-right">
+                                    <td class="text-end"><?= number_format($product['total_qty']) ?></td>
+                                    <td class="text-end">฿<?= number_format($product['total_sales'], 2) ?></td>
+                                    <td class="text-end">฿<?= number_format($product['avg_price'], 2) ?></td>
+                                    <td class="text-end">฿<?= number_format($product['cost_price'], 2) ?></td>
+                                    <td class="text-end">฿<?= number_format($product['profit'], 2) ?></td>
+                                    <td class="text-end">
                                         <?php
                                         $profitPercent = $product['total_sales'] > 0 ?
                                             ($product['profit'] / $product['total_sales']) * 100 : 0;
                                         ?>
-                                        <span class="label label-<?= $profitPercent > 20 ? 'success' : ($profitPercent > 10 ? 'warning' : 'danger') ?>">
+                                        <span class="badge bg-<?= $profitPercent > 20 ? 'success' : ($profitPercent > 10 ? 'warning' : 'danger') ?>">
                                             <?= number_format($profitPercent, 2) ?>%
                                         </span>
                                     </td>
@@ -187,12 +187,12 @@ $this->registerJsFile('https://code.highcharts.com/modules/exporting.js', ['depe
                             <tfoot>
                             <tr class="active">
                                 <th colspan="2">รวมทั้งหมด</th>
-                                <th class="text-right"><?= number_format($totalQty) ?></th>
-                                <th class="text-right">฿<?= number_format($totalSales, 2) ?></th>
+                                <th class="text-end"><?= number_format($totalQty) ?></th>
+                                <th class="text-end">฿<?= number_format($totalSales, 2) ?></th>
                                 <th colspan="2"></th>
-                                <th class="text-right">฿<?= number_format($totalProfit, 2) ?></th>
-                                <th class="text-right">
-                                    <span class="label label-primary"><?= number_format($profitMargin, 2) ?>%</span>
+                                <th class="text-end">฿<?= number_format($totalProfit, 2) ?></th>
+                                <th class="text-end">
+                                    <span class="badge bg-primary"><?= number_format($profitMargin, 2) ?>%</span>
                                 </th>
                             </tr>
                             </tfoot>
