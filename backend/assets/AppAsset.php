88ff961e663375b76fc60bc6e3fc6a8a40ba3592
Niran Wangyad <niran.w@cicnetgrp.net>
2026-08-21 20:59:18 +0700

    update

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -20,5 +20,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap5\BootstrapAsset',
+        'yii\bootstrap5\BootstrapPluginAsset',
     ];
 }
